e438578e34b856b671d325778f78b0fe26a2028a
CamiiloAF <CamiiloAF@users.noreply.github.com>
2020-06-08 18:23:22 +0000

    deploy: 260545c22468ac2a94d064120e8bb904e3af8259

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,14 +1,15 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "e1af7d2887fe6221f974517ad8fc4afd",
+  "main.dart.js": "95d62022e3d0fb9790abc7055fc0c9d4",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
 "assets/LICENSE": "4fea29ab44d33f891f8a35048fdafdb9",
 "assets/packages/progress_dialog/assets/double_ring_loading_io.gif": "e5b006904226dc824fdb6b8027f7d930",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/assets/img/psp.png": "0c1cc248118b972df0baaa4419936ee2",
 "assets/assets/svg/psp.svg": "300a1f7b731071b75c74aa2a8a12c985",
-"assets/AssetManifest.json": "b1b642d95a9dcc37bb92635de2231ad2",
+"assets/AssetManifest.json": "d77f84a53aa6d147d2b09bd73025c1c3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "da11fb44df03bbcd099c256a0577dfcb",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",

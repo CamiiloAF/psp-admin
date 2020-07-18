3c6622c7ec324ed3c23a09a3428b4e7215f73392
CamiiloAF <CamiiloAF@users.noreply.github.com>
2020-07-18 21:41:29 +0000

    deploy: 90ffa54840b31303a4a3b4d6b515ded70f7075d7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "a78dc3392c9b4fe8194415dfe79bcfdd",
+  "main.dart.js": "90fdd4d7d48d7327e4e682c18c0e7fec",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "da11fb44df03bbcd099c256a0577dfcb",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
